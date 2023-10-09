35084d9b05480a7a50e0b967cae4832bf3ecc95a
Jonas Hellgren <jonas.hellgren@ri.se>
2023-10-09 17:33:04 +0200

    point osf xlist

--- src/main/java/optimization_apache/models/SumOfThreeModel.java
+++ src/main/java/optimization_apache/models/SumOfThreeModel.java
@@ -29,8 +29,8 @@ public class SumOfThreeModel implements OptimizationModelInterface {
 
 
     @Override
-     public double getObjective(double[] xList) {
-        return A0 * xList[0] + A1 * xList[1] + A2 * xList[2];
+     public double getObjective(double[] point) {
+        return A0 * point[0] + A1 * point[1] + A2 * point[2];
     }
 
     /**
@@ -38,21 +38,21 @@ public class SumOfThreeModel implements OptimizationModelInterface {
      */
 
     @Override
-    public double getPenalty(double[] xList) {
-        int nofVars = xList.length;
-        double[] lowerBoundsConstrValues = BoundConstraints.getLowerBoundConstraintValues(xList, LB);
-        double[] upperBoundsConstrValues = BoundConstraints.getUpperBoundConstraintValues(xList, UB);
+    public double getPenalty(double[] point) {
+        int nofVars = point.length;
+        double[] lowerBoundsConstrValues = BoundConstraints.getLowerBoundConstraintValues(point, LB);
+        double[] upperBoundsConstrValues = BoundConstraints.getUpperBoundConstraintValues(point, UB);
         List<Double> penalties = new ArrayList<>();
         for (int i = 0; i < nofVars; i++) {
             penalties.add(barrier.process(lowerBoundsConstrValues[i]));
             penalties.add(barrier.process(upperBoundsConstrValues[i]));
         }
-        penalties.add(barrier.process(getSumOfVarsConstraintValue(xList)));
+        penalties.add(barrier.process(getSumOfVarsConstraintValue(point)));
         return ListUtils.sumList(penalties);
     }
 
-    private static double getSumOfVarsConstraintValue(double[] xList) {
-        double sum = ArrayUtil.sum(xList);
+    private static double getSumOfVarsConstraintValue(double[] point) {
+        double sum = ArrayUtil.sum(point);
         return sum - SUM_MAX;
     }
 
